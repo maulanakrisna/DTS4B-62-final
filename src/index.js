66c906637968fcbfaa7eab53bcb8293769d532d7
maulanakrisna <maulanakrisna@hp-probook-440-g6.pln.co.id>
2022-10-27 15:46:09 +0800

    20-add-404-and-redirects

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import SignIn from './containers/SignIn';
 import SignUp from './containers/SignUp';
 import ProtectedRoute from './components/ProtectedRoute';
+import { Box } from '@mui/material';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -23,9 +24,26 @@ root.render(
           </ProtectedRoute>} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
-        <Route path="*" element={<ProtectedRoute>
-          <App />
-          </ProtectedRoute>} />
+        <Route
+          path="*"
+          element={
+            <Box sx={{
+              display: 'flex',
+              margin: 10,
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+            }}>
+          <img
+            src="https://cdn3d.iconscout.com/3d/premium/thumb/404-error-4461124-3696774.png"
+            alt="404"
+          />
+          <p>You have reach the edge of universe</p>
+          <Link to="/">Take me home!</Link>
+        </Box>
+      }
+    />
+      
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
